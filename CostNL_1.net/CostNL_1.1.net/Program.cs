55c6a252ff41bd8997181cf6a5e494f4c104f001
Максим Олександрович Зенченко <zenchmax@gmail.com>
2024-12-16 13:04:16 +0200

    Добавьте файлы проекта.

---
+++ CostNL_1.net/CostNL_1.1.net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CostNL_1._1.net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
