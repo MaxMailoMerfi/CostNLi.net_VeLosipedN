4a4e69d785953d456ae2cfcf86d5b2f4f1fd3288
Максим Олександрович Зенченко <zenchmax@gmail.com>
2024-12-19 01:25:36 +0200

    Завершенно Два массива — два противника

--- CostNL_3.net/CostNL_3.4.net/Program.cs
+++ CostNL_3.net/CostNL_3.4.net/Program.cs
@@ -11,9 +11,82 @@
 
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int length;
+            while (true)
+            {
+                Console.Write("Введите длину масивов:");
+                string input = Console.ReadLine();
+                // Проверка на дурака
+                if (!int.TryParse(input, out length))
+                {
+                    Console.WriteLine("Некорректный ввод");
+                    Console.WriteLine("Нажмите на любую клавишу");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                break;
+            }
 
+            int[] arrayOne = new int[length];
+            int[] arrayTwo = new int[length];
+            int[] arrayThree = new int[length];
+
+            Console.Write("Массив 1 : [");
+            RandomArray(ref arrayOne);
+
+            Console.Write("Массив 2 : [");
+            RandomArray(ref arrayTwo);
+
+            Console.Write("Массив 3 : [");
+            for (int i = 0; i < arrayThree.Length; i++)
+            {
+                arrayThree[i] = arrayOne[i] - arrayTwo[i]; // ришэние
+                if (arrayThree[i] < 0)
+                {
+                    arrayThree[i] = 0;
+                }
+                /*// теж можливий код
+                if (arrayOne[i] > arrayTwo[i])
+                {
+                    arrayThree[i] = arrayOne[i] - arrayTwo[i];
+                }
+                else
+                {
+                    arrayThree[i] = 0;
+                }
+                */
+                if (i == 0)
+                {
+                    Console.Write(arrayThree[i]);
+                }
+                else
+                {
+                    Console.Write(", " + arrayThree[i]);
+                }
+            }
+            Console.WriteLine("]");
+        }
+
+        static void RandomArray(ref int[] array)
+        {
+            Random rnd = new();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(101);
+                if (i == 0)
+                {
+                    Console.Write(array[i]);
+                }
+                else
+                {
+                    Console.Write(", " + array[i]);
+                }
+            }
+            Console.WriteLine("]");
         }
     }
 }
