b41b00207a8d160cc0eefe4d785498390bcb7642
Максим Олександрович Зенченко <zenchmax@gmail.com>
2024-12-20 11:55:14 +0200

    Завершенно Массив для шифра Цезаря

--- CostNL_3.net/CostNL_3.5.net/Program.cs
+++ CostNL_3.net/CostNL_3.5.net/Program.cs
@@ -11,32 +11,96 @@
     {
         static void Main()
         {
-            int shift = 3, i = 0;
-
-            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            char[] input = alphabet.ToCharArray();
-
-            foreach (char c in input)
+            while (true)
             {
-                Console.Write(c + "|");
-            }
-            Console.Write("\n\n | | |");
-            foreach (char c in input)
-            {
-                if (shift <= i || shift > input.Length - shift)
+                int shift = 3;// сдвиг
+
+                Console.Write("Ввод:     ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
                 {
-                    input[i - shift] = c;
+                    Console.Clear();
+                    continue;
                 }
-                else
+
+                char[] result = ShiftCharacters(input,shift);
+
+                Console.WriteLine("Результат:" + new string(result));
+
+                /*
+                int shift = 3;// сдвиг
+
+                int position = 0;
+
+                string result = "ABC";
+
+                char[] result = result.ToCharArray(), a = new char[shift];
+
+                foreach (char c in result)
                 {
-                    input[input.Length - shift + i] = c;
+                    if (shift <= position)
+                    {
+                        if (position > result.Length - shift - 1)
+                        {
+                            result[position - shift] = c;
+                            result[position] = a[position - result.Length + shift];
+                        }
+                        else
+                        {
+                            result[position - shift] = c;
+                        }
+                    }
+                    else
+                    {
+                        a[position] = c;
+                    }
+
+                    position++;
+                    Console.Write(c + "|");
                 }
-                i++;
+
+                Console.Write("\n");
+                Console.WriteLine(new string(result));
+                foreach (char c in result)
+                {
+                    Console.Write(c + "|");
+                }
+                */
+
+
+
+                Console.WriteLine("\n\nЧтобы попробовать еще раз, нажмите на любую клавишу");
+                Console.ReadKey();
+                Console.Clear();
             }
 
-            foreach (char c in input)
+            static char[] ShiftCharacters(string input,int shift)
             {
-                Console.Write(c + "|");
+                char[] result = input.ToCharArray();
+                //Console.WriteLine("Ввод:        " + new string(result));
+                int i = 0;
+                foreach (char c in result)
+                {
+                    if (char.IsUpper(c)) // Большая буква
+                    {
+                        result[i] = (char)('A' + (c - 'A' + shift) % 26);
+                    }
+                    else if (char.IsLower(c)) // Малая буква
+                    {
+                        result[i] = (char)('a' + (c - 'a' + shift) % 26);
+                    }
+                    else if (char.IsDigit(c)) // Число
+                    {
+                        result[i] = (char)('0' + (c - '0' + shift) % 10);
+                    }
+                    else // Остальные
+                    {
+                        result[i] = c;
+                    }
+                    i++;
+                }
+                return result;
             }
         }
     }
